36ccd8fc08b008c6ce123715c966020a9a5b4cbf
Anderson Silva de Sa <asilvadesa@gmail.com>
2022-06-21 22:32:38 -0300

    Understand diference between val and var

---
+++ src/main/kotlin/fundamentals/Variables.kt
@@ -0,0 +1,14 @@
+package fundamentals
+
+fun main() {
+
+    //mutable
+    var myName = "Anderson"
+    myName = "Anderson Silva"
+
+    println("Hello "+ myName)
+
+    //immutable
+    val myNameVal = "Anderson"
+    println("Hello " + myNameVal)
+}
